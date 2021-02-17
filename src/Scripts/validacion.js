8a9177b515a590d01c605aa81b4470e7d2644f32
carlota sas <casaor@outlook.com>
2021-02-17 19:47:53 +0100

    validaciones hijo

--- src/Scripts/validacion.js
+++ src/Scripts/validacion.js
@@ -11,44 +11,35 @@ export default {
 
     methods: 
     {
-        GetValidation(validacion,typeValidation){
+        GetValidation (validacion,typeValidation){
             if(typeValidation === "text"){
-                if(validacion === ""){
+                if(validacion.length < 6 || validacion.length > 13){
                     this.errors = true;
-                    this.messageError = "El campo no puede estar vacío";
+                    this.messageError = "El nombre debe contener entre 6 y 13 carácteres";
                 }else{
                     this.errors = false;
                 }
             }
-            if(typeValidation === "number"){
+            else if(typeValidation === "number"){
                 if(isNaN(validacion)){
                     this.errors = true;
                     this.messageError = "Este campo debe ser numérico";
-                }else if(validacion === ""){
-                    this.errors = true;
-                    this.messageError = "El campo no puede estar vacío";
                 }else{
                     this.errors = false;
                 }
             }
-            if(typeValidation === "email"){
-                if(validacion === ""){
-                    this.errors = true;
-                    this.messageError = "El campo no puede estar vacío";
-                }else if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(validacion)){
+            else if(typeValidation === "email"){
+                if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(validacion)){
                     this.errors = true;
                     this.messageError = "El formato de email no es válido";
                 }else{
                     this.errors = false;
                 }
             }
-            if(typeValidation === "password"){
-                if(validacion === ""){
+            else if(typeValidation === "password"){
+                if(validacion.length < 6 || validacion.length > 13){
                     this.errors = true;
-                    this.messageError = "El campo no puede estar vacío";
-                }else if(validacion.lenght < 6 || validacion.lenght > 13){
-                    this.errors = true;
-                    this.messageError = "El password debe contener más de 6 y menos de 13 carácteres";
+                    this.messageError = "El password debe contener entre 6 y 13 carácteres";
                 }else if(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/.test(validacion)){
                     this.errors = true;
                     this.messageError = "El password debe contener mayúsculas, minúsculas y algún número";
